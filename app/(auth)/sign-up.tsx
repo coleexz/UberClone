bec708d51ad0c7d42ade99f83f7cc7e4e4a0dafb
coleexz <williamstevencole@gmail.com>
2025-02-03 11:36:55 -0600

    Finished sign-up window

--- app/(auth)/sign-up.tsx
+++ app/(auth)/sign-up.tsx
@@ -1,11 +1,62 @@
 import React from "react";
-import { View, Text } from "react-native";
+import { View, Text, Image, ScrollView } from "react-native";
+import InputField from "@/components/InputField";
+import { images } from "@/constants";
+import { icons } from "@/constants";
+import { useState } from "react";
+
+import CustomButton from "@/components/CustomButton";
+import OAuth from "@/components/OAuth";
 
 const SignUp = () => {
+  const [form, setForm] = useState({
+    name: "",
+    email: "",
+    password: "",
+  });
+
   return (
-    <View className="w-screen h-screen flex-1 justify-center items-center">
-      <Text className="">SignIn</Text>
-    </View>
+    <ScrollView className="flex-1 bg-white  ">
+      <View className="relative w-full h-[250px]">
+        <Image source={images.signUpCar} className="z-0 w-full h-[250px]" />
+        <Text className="text-3xl text-black font-JakartaBold absolute bottom-5 left-5">
+          Create Your Account
+        </Text>
+      </View>
+
+      <View className="p-5">
+        <InputField
+          label="Name"
+          placeholder="Enter name"
+          icon={icons.person}
+          value={form.name}
+          onChangeText={(value) => setForm({ ...form, name: value })}
+        />
+        <InputField
+          label="Email"
+          placeholder="Enter email"
+          icon={icons.email}
+          textContentType="emailAddress"
+          value={form.email}
+          onChangeText={(value) => setForm({ ...form, email: value })}
+        />
+        <InputField
+          label="Password"
+          placeholder="Enter password"
+          icon={icons.lock}
+          secureTextEntry={true}
+          textContentType="password"
+          value={form.password}
+          onChangeText={(value) => setForm({ ...form, password: value })}
+        />
+
+        <CustomButton title="Sign Up" className="mt-8 mb-4 " />
+        <OAuth />
+        <Text className="flex-1 text-center font-JakartaSemiBold text-[#858585]">
+          Already have an account? <Text className="text-[#0286FF]">Login</Text>
+        </Text>
+      </View>
+    </ScrollView>
   );
 };
 
